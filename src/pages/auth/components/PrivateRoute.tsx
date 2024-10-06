e6309301690d5bc1bd968289fff740e0d82f871f
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-10-06 20:45:04 +0300

    fix: rm session

--- src/pages/auth/components/PrivateRoute.tsx
+++ src/pages/auth/components/PrivateRoute.tsx
@@ -1,21 +1,9 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const SESSION_TIMEOUT = 100 * 60 * 1000;
+import { Outlet, Navigate } from 'react-router-dom';
 
 function PrivateRoute() {
     const token = localStorage.getItem('token');
-    const loginTime = localStorage.getItem('loginTime');
 
-    if (token && loginTime) {
-        const currentTime = new Date().getTime();
-        const timeSinceLogin = currentTime - parseInt(loginTime, 10);
-
-        if (timeSinceLogin > SESSION_TIMEOUT) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('loginTime');
-            return <Navigate to="/" />;
-        }
-    } else {
+    if (!token) {
         return <Navigate to="/" />;
     }
 
